d120bf7604ed05e6515fc99544b124419db6f2c8
MrDaft <daft.93@gmail.com>
2021-09-27 20:15:19 +0300

    classified mobile report phone_show

--- bq_reports/classified_report.py
+++ bq_reports/classified_report.py
@@ -15,17 +15,21 @@ credentials = '/home/web_analytics/m2-main-cd9ed0b4e222.json'
 gc = pygsheets.authorize(service_file=credentials)
 g_auth_service = service_account.Credentials.from_service_account_file(credentials)
 bq_client = bigquery.Client(credentials=g_auth_service)
+
 # spreadsheets
 classified_mobile = '1dojfyD0BHErfUx0YpeqsNna33JEPqABMmBaYceDjsqw'
-sheet = 'import_all'
-# views
+sheet_all = 'import_all'
+sheet_phone_call = 'bq_Classified'
+
+# bq views
 all_events = 'm2-main.mobile_apps.view_classified_mobile_all_events'
+phone_call = 'm2-main.mobile_apps.view_classified_mobile_show_and_call'
 
 
 def send_to_ss(spreadsheet, worksheet, dataframe, start_cell):
     sh = gc.open_by_key(spreadsheet)
     wks = sh.worksheet_by_title(worksheet)
-    wks.set_dataframe(dataframe, start=start_cell, copy_head=False, encoding="utf-8-sig")
+    wks.set_dataframe(dataframe, start=start_cell, copy_head=False, extend=True, encoding="utf-8-sig")
 
 
 def bq_get_view(view_id, client=bq_client):
@@ -40,8 +44,17 @@ def get_gs_len(spreadsheet, worksheet):
     return len.shape
 
 
+# import all
 try:
     df = pd.read_gbq(bq_get_view(all_events), credentials=g_auth_service)
-    send_to_ss(classified_mobile, sheet, df, f'A{int(get_gs_len(classified_mobile,sheet)[0])+2}')
+    send_to_ss(classified_mobile, sheet_all, df, f'A{int(get_gs_len(classified_mobile, sheet_all)[0])+2}')
+except Exception as e:
+    logging.exception(str(e))
+
+
+# bq_classified
+try:
+    df = pd.read_gbq(bq_get_view(phone_call), credentials=g_auth_service)
+    send_to_ss(classified_mobile, sheet_phone_call, df, f'A{int(get_gs_len(classified_mobile,sheet_phone_call)[0])+2}')
 except Exception as e:
     logging.exception(str(e))
