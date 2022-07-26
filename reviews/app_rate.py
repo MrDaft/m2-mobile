f43793081967cc58ff2a651c4b0a0a9d993789cc
MrDaft <daft.93@gmail.com>
2022-07-26 19:09:31 +0300

    test

--- reviews/app_rate.py
+++ reviews/app_rate.py
@@ -5,16 +5,26 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 from google_play_scraper import app
 import os
+from clickhouse_driver import Client
 
 # pandas settings
 pd.set_option('display.max_rows', None)  # show all rows in terminal
 pd.set_option('display.expand_frame_repr', False)  # show all columns in terminal
 
 # settings
-credentials = os.environ.get('GOOGLE_CLOUD_CREDENTIALS')
+host = os.environ.get('CH_HOST')
+user = os.environ.get('CH_USER')
+password = os.environ.get('CH_PWD')
+database = os.environ.get('CH_DBWA')
 
-g_auth_service = service_account.Credentials.from_service_account_file(credentials)
-bq_client = bigquery.Client(credentials=g_auth_service)
+client = Client(host=host, port='9000', user=user, password=password, database=database, settings={'use_numpy': True})
+
+# g_auth_service = service_account.Credentials.from_service_account_file(credentials)
+# bq_client = bigquery.Client(credentials=g_auth_service)
+
+client.execute(
+"CREATE TABLE IF NOT EXISTS test (os String, app String, rating Float64, rating_count Int64, date Date) Engine = Memory"
+)
 
 android_app_id = {'ru.m2.squaremeter': 'Метр Квадратный',
                   'ru.cian.main': 'Циан',
@@ -61,14 +71,16 @@ df['rating'] = ratings
 df['rating_count'] = rating_count
 df['date'] = today
 
-df.to_gbq(
-    destination_table='mobile_app_data.rating',
-    project_id='m2-main',
-    if_exists='append',
-    credentials=g_auth_service,
-    table_schema=[{'name': 'os', 'type': 'STRING'},
-                  {'name': 'app', 'type': 'STRING'},
-                  {'name': 'rating', 'type': 'FLOAT'},
-                  {'name': 'rating_count', 'type': 'INTEGER'},
-                  {'name': 'date', 'type': 'DATE'}]
-)
+# df.to_gbq(
+#     destination_table='mobile_app_data.rating',
+#     project_id='m2-main',
+#     if_exists='append',
+#     credentials=g_auth_service,
+#     table_schema=[{'name': 'os', 'type': 'STRING'},
+#                   {'name': 'app', 'type': 'STRING'},
+#                   {'name': 'rating', 'type': 'FLOAT'},
+#                   {'name': 'rating_count', 'type': 'INTEGER'},
+#                   {'name': 'date', 'type': 'DATE'}]
+# )
+
+client.insert_dataframe("INSERT INTO test VALUES", df)
