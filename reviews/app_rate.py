edcb373f2b54795f0560a9c0a2dbbdc62555f4dc
MrDaft <daft.93@gmail.com>
2022-07-27 16:14:12 +0300

    test

--- reviews/app_rate.py
+++ reviews/app_rate.py
@@ -22,62 +22,62 @@ except Exception:
     exit()
 
 
-# pandas settings
-pd.set_option('display.max_rows', None)  # show all rows in terminal
-pd.set_option('display.expand_frame_repr', False)  # show all columns in terminal
-
-
-
-client = Client(host=cred['CH_HOST'], port='9000', user=cred['CH_USER'], password=cred['CH_PWD'], database=cred['CH_DBWA'], settings={'use_numpy': True})
-
-client.execute(
-"CREATE TABLE IF NOT EXISTS testrewiews (os String, app String, rating Float64, rating_count Int64, date Date) Engine = Memory"
-)
-
-android_app_id = {'ru.m2.squaremeter': 'Метр Квадратный',
-                  'ru.cian.main': 'Циан',
-                  # 'ru.domclick.mortgage': 'ДомКлик',
-                  'com.avito.android': 'Авито'
-                  }
-
-ios_app_id = {'1501340734': 'Метр Квадратный',
-              '911804296': 'Циан',
-              '1143031400': 'ДомКлик',
-              '417281773': 'Авито'}
-
-country = 'RU'
-os = []
-ratings = []
-app_name = []
-rating_count = []
-today = datetime.today().strftime('%Y-%m-%d')
-
-# appstore data
-for key, value in ios_app_id.items():
-    url = f'http://itunes.apple.com/lookup?id={key}&country={country}'
-    r = requests.get(url)
-    req = r.json()
-    rating = req['results']
-    os.append('IOS')
-    rating_count.append(rating[0]['userRatingCount'])
-    app_name.append(value)
-    ratings.append(rating[0]['averageUserRating'])
-
-# googleplay data
-for key, value in android_app_id.items():
-    result = app(key, country=country)
-    os.append('ANDROID')
-    app_name.append(value)
-    rating_count.append(result['ratings'])
-    ratings.append(result['score'])
-
-df = pd.DataFrame()
-df['os'] = os
-df['app'] = app_name
-df['rating'] = ratings
-df['rating_count'] = rating_count
-df['date'] = today
-
-client.insert_dataframe("INSERT INTO testrewiews VALUES", df)
+# # pandas settings
+# pd.set_option('display.max_rows', None)  # show all rows in terminal
+# pd.set_option('display.expand_frame_repr', False)  # show all columns in terminal
+#
+#
+#
+# client = Client(host=cred['CH_HOST'], port='9000', user=cred['CH_USER'], password=cred['CH_PWD'], database=cred['CH_DBWA'], settings={'use_numpy': True})
+#
+# client.execute(
+# "CREATE TABLE IF NOT EXISTS testrewiews (os String, app String, rating Float64, rating_count Int64, date Date) Engine = Memory"
+# )
+#
+# android_app_id = {'ru.m2.squaremeter': 'Метр Квадратный',
+#                   'ru.cian.main': 'Циан',
+#                   # 'ru.domclick.mortgage': 'ДомКлик',
+#                   'com.avito.android': 'Авито'
+#                   }
+#
+# ios_app_id = {'1501340734': 'Метр Квадратный',
+#               '911804296': 'Циан',
+#               '1143031400': 'ДомКлик',
+#               '417281773': 'Авито'}
+#
+# country = 'RU'
+# os = []
+# ratings = []
+# app_name = []
+# rating_count = []
+# today = datetime.today().strftime('%Y-%m-%d')
+#
+# # appstore data
+# for key, value in ios_app_id.items():
+#     url = f'http://itunes.apple.com/lookup?id={key}&country={country}'
+#     r = requests.get(url)
+#     req = r.json()
+#     rating = req['results']
+#     os.append('IOS')
+#     rating_count.append(rating[0]['userRatingCount'])
+#     app_name.append(value)
+#     ratings.append(rating[0]['averageUserRating'])
+#
+# # googleplay data
+# for key, value in android_app_id.items():
+#     result = app(key, country=country)
+#     os.append('ANDROID')
+#     app_name.append(value)
+#     rating_count.append(result['ratings'])
+#     ratings.append(result['score'])
+#
+# df = pd.DataFrame()
+# df['os'] = os
+# df['app'] = app_name
+# df['rating'] = ratings
+# df['rating_count'] = rating_count
+# df['date'] = today
+#
+# client.insert_dataframe("INSERT INTO testrewiews VALUES", df)
 
 
