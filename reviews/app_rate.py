4e241cad7f86f86a123ec76bc0fd2a070506df8c
MrDaft <daft.93@gmail.com>
2022-07-27 16:03:38 +0300

    test

--- reviews/app_rate.py
+++ reviews/app_rate.py
@@ -1,26 +1,34 @@
 import requests
 import pandas as pd
 from datetime import datetime
-# from google.oauth2 import service_account
-# from google.cloud import bigquery
 from google_play_scraper import app
 import os
 from clickhouse_driver import Client
+import json
+
+
+# get creds
+try:
+    if os.environ.get('CH_HOST') is not None:
+        host = os.environ.get('CH_HOST')
+        user = os.environ.get('CH_USER')
+        password = os.environ.get('CH_PWD')
+        database = os.environ.get('CH_DBWA')
+    else:
+        with open('~/creds.json') as f:
+            cred = json.load(f)
+            print(cred)
+except Exception:
+    exit()
+
 
 # pandas settings
 pd.set_option('display.max_rows', None)  # show all rows in terminal
 pd.set_option('display.expand_frame_repr', False)  # show all columns in terminal
 
-# settings
-host = os.environ.get('CH_HOST')
-user = os.environ.get('CH_USER')
-password = os.environ.get('CH_PWD')
-database = os.environ.get('CH_DBWA')
 
-client = Client(host=host, port='9000', user=user, password=password, database=database, settings={'use_numpy': True})
 
-# g_auth_service = service_account.Credentials.from_service_account_file(credentials)
-# bq_client = bigquery.Client(credentials=g_auth_service)
+client = Client(host=cred['CH_HOST'], port='9000', user=cred['CH_USER'], password=cred['CH_PWD'], database=cred['CH_DBWA'], settings={'use_numpy': True})
 
 client.execute(
 "CREATE TABLE IF NOT EXISTS testrewiews (os String, app String, rating Float64, rating_count Int64, date Date) Engine = Memory"
@@ -57,7 +65,6 @@ for key, value in ios_app_id.items():
 
 # googleplay data
 for key, value in android_app_id.items():
-    print(f'{key}')
     result = app(key, country=country)
     os.append('ANDROID')
     app_name.append(value)
@@ -71,16 +78,6 @@ df['rating'] = ratings
 df['rating_count'] = rating_count
 df['date'] = today
 
-# df.to_gbq(
-#     destination_table='mobile_app_data.rating',
-#     project_id='m2-main',
-#     if_exists='append',
-#     credentials=g_auth_service,
-#     table_schema=[{'name': 'os', 'type': 'STRING'},
-#                   {'name': 'app', 'type': 'STRING'},
-#                   {'name': 'rating', 'type': 'FLOAT'},
-#                   {'name': 'rating_count', 'type': 'INTEGER'},
-#                   {'name': 'date', 'type': 'DATE'}]
-# )
-
 client.insert_dataframe("INSERT INTO testrewiews VALUES", df)
+
+
