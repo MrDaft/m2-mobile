c1d59f56a8c40baceb5ff5ec9716d23356699e0a
MrDaft <daft.93@gmail.com>
2022-08-02 14:10:52 +0300

    test

--- reviews/app_rate.py
+++ reviews/app_rate.py
@@ -6,7 +6,6 @@ import os
 from clickhouse_driver import Client
 import json
 
-requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=started')
 # get creds
 try:
     if os.environ.get('CH_HOST') is not None:
@@ -15,7 +14,6 @@ try:
         password = os.environ.get('CH_PWD')
         database = os.environ.get('CH_DBWA')
         print('environ', host, user, password, database)
-        requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=environ')
 
     else:
         with open('/home/nikitindd/creds.json') as f:
@@ -24,8 +22,7 @@ try:
             user = cred['CH_USER']
             password = cred['CH_PWD']
             database = cred['CH_DBWA']
-            print('creds',host, user, password, database)
-            requests.get(                'https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=creds')
+            print('creds', host, user, password, database)
 except Exception:
     print('no cred')
     # exit()
@@ -38,11 +35,9 @@ pd.set_option('display.expand_frame_repr', False)  # show all columns in termina
 
 
 client = Client(host=host, port='9000', user=user, password=password, database=database, settings={'use_numpy': True})
-requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=clien_passed')
 client.execute(
 "CREATE TABLE IF NOT EXISTS testrewiews (os String, app String, rating Float64, rating_count Int64, date Date) Engine = Memory"
 )
-requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=passed_create_table')
 android_app_id = {'ru.m2.squaremeter': 'Метр Квадратный',
                   'ru.cian.main': 'Циан',
                   # 'ru.domclick.mortgage': 'ДомКлик',
@@ -86,7 +81,7 @@ df['app'] = app_name
 df['rating'] = ratings
 df['rating_count'] = rating_count
 df['date'] = today
-requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=data_collected')
-client.insert_dataframe("INSERT INTO testrewiews VALUES", df)
-requests.get('https://api.telegram.org/bot718743630:AAF_iyWf_VmNXRq8a_Ofpt1fhFRZyDOFW3s/sendMessage?chat_id=252816148&text=inserted')
 
+client.insert_dataframe("INSERT INTO testrewiews VALUES", df)
+
+client.query_dataframe('SELECT number AS x, (number + 100) AS y FROM system.numbers LIMIT 10000')
